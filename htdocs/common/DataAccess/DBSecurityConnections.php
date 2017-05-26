730b5f7eda8395fed48c9b1492e64457c8735b9e
Sebastian Portesi <sebastian.nahuel.portesi@gmail.com>
2017-05-26 11:09:34 -0300

    .

--- htdocs/common/DataAccess/DBSecurityConnections.php
+++ htdocs/common/DataAccess/DBSecurityConnections.php
@@ -1,5 +1,5 @@
 <?php	
-    header('Content-type: text/plain; charset=utf-8');
+    header('Content-type: text/html; charset=utf-8');
 
 	require_once(__ROOT__.'/common/DataAccess/BDSettings.php'); 
 	
