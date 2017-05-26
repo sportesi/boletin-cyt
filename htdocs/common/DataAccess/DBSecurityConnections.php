07aa46a71ef5e89738ce77982fb3362bed57d4e3
Sebastian Portesi <sebastian.nahuel.portesi@gmail.com>
2017-05-26 11:08:52 -0300

    .

--- htdocs/common/DataAccess/DBSecurityConnections.php
+++ htdocs/common/DataAccess/DBSecurityConnections.php
@@ -1,4 +1,6 @@
 <?php	
+    header('Content-type: text/plain; charset=utf-8');
+
 	require_once(__ROOT__.'/common/DataAccess/BDSettings.php'); 
 	
 	$dbInformation = new DBInformation("127.0.0.1","ees_db","ees_uai","QERA7854");
