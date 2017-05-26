f3570420f4b68f9f197d573ee06a79673bc6a46a
Sebastian Portesi <sebastian.nahuel.portesi@gmail.com>
2017-05-26 11:48:58 -0300

    .

--- htdocs/news/operation.php
+++ htdocs/news/operation.php
@@ -190,8 +190,29 @@ function GetNews($dbSetting)
 			}
 			
 			echo json_encode($result);
-			if (json_last_error()) {
-				echo json_last_error();
+	
+			switch(json_last_error()) {
+				case JSON_ERROR_NONE:
+				echo ' - Sin errores';
+				break;
+				case JSON_ERROR_DEPTH:
+				echo ' - Excedido tamaño máximo de la pila';
+				break;
+				case JSON_ERROR_STATE_MISMATCH:
+				echo ' - Desbordamiento de buffer o los modos no coinciden';
+				break;
+				case JSON_ERROR_CTRL_CHAR:
+				echo ' - Encontrado carácter de control no esperado';
+				break;
+				case JSON_ERROR_SYNTAX:
+				echo ' - Error de sintaxis, JSON mal formado';
+				break;
+				case JSON_ERROR_UTF8:
+				echo ' - Caracteres UTF-8 malformados, posiblemente están mal codificados';
+				break;
+				default:
+				echo ' - Error desconocido';
+				break;
 			}
 	   }
 	   catch (Exception $e)
