0a4d834a4239e3a2c65703d02ac7a88180ae6e2e
yippiekayeay <sebastian.portesi@outlook.com>
2017-06-27 18:43:12 -0300

    Master refactor 2 (#14)
    * cambie como se traen las noticias en la home
    
    * elimine la carpeta .idea
    
    * instalado bootstrap por npm'
    '
    
    * rediseñadas las noticias
    
    * ajustes al diseño de las noticias
    
    * rediseño general de la homepage
    
    * terminado rediseño de pagina principal
    
    * Test
    
    * removed test file

---
+++ controls/menu/menu_nav.php
@@ -0,0 +1,78 @@
+<nav class="navbar navbar-default">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="/">Boletín CyT</a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+      <ul class="nav navbar-nav">
+        <!-- <li class="active"><a href="#">Link <span class="sr-only">(current)</span></a></li> -->
+        <li><a href="/news/top/">Noticias Top</a></li>
+        <?php $query_str = "?year=" . date('Y') . "&cuatrimestre=" . ((date('m') > 8) ? 2 : 1); ?>
+        <?php if ($session->GetSessionValue('valid') == 'valid'): ?>
+          <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              Configuración de Usuario <span class="caret"></span>
+            </a>
+            <ul class="dropdown-menu">
+              <li><a href="/user/update/index.php">Actualizar Datos</a></li>
+              <li><a href="/user/change_password/index.php">Cambiar Contraseña</a></li>
+            </ul>
+          </li>
+
+          <?php if ($session->GetSessionValue('permission') == 2): ?>
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                Cursos <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="/admin/users/report/courses/index.php<?php echo $query_str; ?>">Reporte</a></li>
+                <li><a href="/admin/users/validate/index.php<?php echo $query_str; ?>">Validar Alumnos</a></li>
+              </ul>
+            </li>
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                Categorías <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="/admin/category/create/index.php">Crear</a></li>
+                <li><a href="/admin/category/list/index.php">Listar</a></li>
+              </ul>
+            </li>
+
+            <li><a href="/admin/users/news/list/index.php?offset=0&pageperview=10">Listar Noticias</a></li>
+          <?php else: ?>
+            <li><a href="/user/statistics/index.php?id=<?php echo $session->GetSessionValue('user_id') ?>">Rendimiento</a></li>
+
+            <li class="dropdown">
+              <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                Noticias <span class="caret"></span>
+              </a>
+              <ul class="dropdown-menu">
+                <li><a href="/news/create">Crear</a></li>
+                <li><a href="/news/list/">Lista por usuario</a></li>
+              </ul>
+            </li>
+          <?php endif; ?>
+        <?php endif; ?>
+      </ul>
+      <div class="navbar-right">
+        <?php if ($session->GetSessionValue('valid') == 'valid'): ?>
+          <a href="/login/logout.php" class="btn btn-primary btn-sm navbar-btn">Salir</a>
+        <?php else: ?>
+          <a href="/user/register/" class="btn btn-success btn-sm navbar-btn">Registrarse</a>
+          <a href="/login/" class="btn btn-primary btn-sm navbar-btn">Ingresar</a>
+        <?php endif; ?>
+      </div>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
---
+++ news/index_view.php
@@ -0,0 +1,110 @@
+<?php
+
+$category_id = DBInformation::mysql_escape_mimic(filter_input(INPUT_GET, "category"));
+$user_id = DBInformation::mysql_escape_mimic(filter_input(INPUT_GET, "user_id"));
+$news_id = DBInformation::mysql_escape_mimic(filter_input(INPUT_GET, "id"));
+$offset = DBInformation::mysql_escape_mimic(filter_input(INPUT_GET, "offset"));
+$pageperview = DBInformation::mysql_escape_mimic(filter_input(INPUT_GET, "pageperview"));
+
+$query = "SELECT N.id,N.user_id, CONCAT_WS(', ',U.firstname,U.lastname) 'fullname', C.id 'campus_id', C.name 'campus', U.year 'year_coursed', T.id 'turn_id', T.name 'turn', U.comission 'comission', N.title, N.sub_title, N.summary, N.sub_summary, N.image_url, N.image_comment, N.category_id, Cat.name 'category', N.link_1, N.link_2, N.link_3, N.date FROM news N, user U, category Cat, campus C, turn T WHERE N.category_id = Cat.id AND N.user_id = U.id AND U.campus_id = C.id AND U.turn_id = T.id";
+
+if ($category_id != '') { $query = $query . " AND N.category_id = " . $category_id; }
+if ($user_id != '') { $query = $query . " AND N.user_id = " . $user_id; }
+if ($news_id != '') { $query = $query . " AND N.id = " . $news_id; }
+
+$query = $query . " ORDER BY N.date DESC";
+$query = $query . " LIMIT " . ($offset ?: 0) . " , " . ($pageperview ?: 3) . " ";
+$rs = $dbSetting->ExecuteQuery($query);
+$result = array();
+
+for ($x = 0, $numrows = mysql_num_rows($rs); $x < $numrows; $x++) {
+  $row = mysql_fetch_assoc($rs);
+
+  $result[$x] = array(
+    "id" => $row["id"],
+    "user_id" => $row["user_id"],
+    "fullname" => $row["fullname"],
+    "campus_id" => $row["campus_id"],
+    "campus" => $row["campus"],
+    "year_coursed" => $row["year_coursed"],
+    "turn_id" => $row["turn_id"],
+    "turn" => $row["turn"],
+    "comission" => $row["comission"],
+    "title" => $row["title"],
+    "sub_title" => $row["sub_title"],
+    "summary" => $row["summary"],
+    "sub_summary" => $row["sub_summary"],
+    "image_url" => $row["image_url"],
+    "image_comment" => $row["image_comment"],
+    "category_id" => $row["category_id"],
+    "category" => $row["category"],
+    "link_1" => $row["link_1"],
+    "link_2" => $row["link_2"],
+    "link_3" => $row["link_3"],
+    "date" => $row["date"]
+  );
+}
+
+?>
+
+<?php foreach ($result as $data) { ?>
+
+  <div class="panel panel-info">
+    <div class="panel-body no-padding">
+      <div class="col-md-3">
+        <img class="img-thumbnail" src="<?php echo $data['image_url']; ?>" onerror="ChangeImg(this);"/>
+      </div>
+      <div class="col-md-9">
+        <div class="panel panel-info">
+          <div class="panel-heading">
+            <b> <?php echo htmlentities($data['title'], ENT_QUOTES, 'utf-8'); ?> </b>
+          </div>
+          <div class="panel-body">
+            <p>
+              <?php echo htmlentities($data['sub_summary'], ENT_QUOTES, 'utf-8'); ?>
+            </p>
+            <p>
+              <?php echo htmlentities($data['summary'], ENT_QUOTES, 'utf-8'); ?>
+            </p>
+            <div class="text-center">
+              <?php if (!empty($data['link_1'])): ?>
+                <a href="/news/preview/preview.php?news_id=<?php echo $data['id']; ?>&link=1" class="btn btn-info btn-sm">Nota Completa</a>
+              <?php endif; ?>
+              <?php if (!empty($data['link_2'])): ?>
+                <a href="/news/preview/preview.php?news_id=<?php echo $data['id']; ?>&link=2" class="btn btn-info btn-sm">Relacionado</a>
+              <?php endif; ?>
+              <?php if (!empty($data['link_3'])): ?>
+                <a href="/news/preview/preview.php?news_id=<?php echo $data['id']; ?>&link=3" class="btn btn-info btn-sm">Formato PDF</a>
+              <?php endif; ?>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="panel-footer">
+      <div class="row">
+        <div class="col-md-5">
+          <strong>Autor:&nbsp;</strong>
+          <a href="/user/statistics/index.php?id=<?php echo $data['user_id']; ?>">
+            <?php echo htmlentities($data['fullname'], ENT_QUOTES, 'utf-8'); ?>
+            (
+            <?php echo htmlentities($data['year_coursed'], ENT_QUOTES, 'utf-8'); ?>
+            <?php echo htmlentities($data['comission'], ENT_QUOTES, 'utf-8'); ?>
+            -
+            <?php echo htmlentities($data['turn'], ENT_QUOTES, 'utf-8'); ?>
+            )
+          </a>
+        </div>
+        <div class="col-md-3 text-center">
+          <strong>Sede:&nbsp;</strong>
+          <?php echo htmlentities($data['campus'], ENT_QUOTES, 'utf-8'); ?>
+        </div>
+        <div class="col-md-4 text-right">
+          <strong>Fecha:&nbsp;</strong>
+          <?php echo htmlentities($data['date'], ENT_QUOTES, 'utf-8'); ?>
+        </div>
+      </div>
+    </div>
+  </div>
+
+<?php } ?>
