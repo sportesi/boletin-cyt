b66bd411a92700ae6d59d67a36706f2ccdce2d57
Sebastian Portesi <sebastian.portesi@outlook.com>
2017-08-14 09:12:03 -0300

    Estilos nuevos y refactorizacion

--- admin/category/list/index.php
+++ admin/category/list/index.php
@@ -6,11 +6,8 @@ require_once(__ROOT__ . '/common/session/Session.php');
 require_once(__ROOT__ . '/common/DataAccess/DBSecurityConnections.php');
 
 //Queries
-define('__QUERY_GET_ALL_CATEGORIES_ORDER_BY_NAME__', 'SELECT 
- 																C.* 
- 														  FROM category C 
- 														  WHERE C.deleted = 0 
- 														  ORDER BY C.name DESC');
+define('__QUERY_GET_ALL_CATEGORIES_ORDER_BY_NAME__',
+    'SELECT C.* FROM category C WHERE C.deleted = 0 ORDER BY C.name DESC');
 
 //Check if the user is still login
 if ($session->GetSessionValue('valid') != 'valid') {
@@ -24,117 +21,97 @@ if ($session->GetSessionValue('permission') < 2) {
 
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
+
 <head>
     <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <?php
-    //Get User to used in the title
-    $title_description = ' - Usuario: ' . $session->GetSessionValue('login_user');
-    echo '<title>EES - UAI - Administrador de Caregorías ' . $title_description . '</title>';
-    ?>
-    <link rel="stylesheet" type="text/css" href="/style/css/general.css" media="screen"/>
-    <style type="text/css" title="currentStyle">
-        @import "/controls/grid/datatables-grid/css/demo_page.css";
-        @import "/controls/grid/datatables-grid/css/demo_table.css";</style>
+    <meta http-equiv="X-UA-Compatible" content="IE=8"/>
+    <meta name="description"
+          content="Electromagnetismo y Estado Solido Universidad Abierta Internacional,
+          EES UAI Joaquin Abraham Wessolowski, oftware, Seguridad Informatica, Salud , Robótica e Inteligenia
+          Artificial,Politicas y Ética, Periféricos y Auxiliares, Otros, Nanotecnologia, Matematica y Lógica, IT &
+          Infraestuctura Informatica, Fuentes RRS varias, Fisica y Quimica, Comunicaciones, Computación Cuantica,
+          Circuitos Integrados, Almacenamiento y Memorias "/>
+    <meta name="keywords"
+          content="Software, Seguridad Informatica, Salud , Robótica e Inteligenia Artificial,Politicas y Ética,
+          Periféricos y Auxiliares, Otros, Nanotecnologia, Matematica y Lógica, IT & Infraestuctura Informatica,
+          Fuentes RRS varias, Fisica y Quimica, Comunicaciones, Computación Cuantica, Circuitos Integrados,
+          Almacenamiento y Memorias "/>
 
-    <script type="text/javascript" language="JavaScript" src="/scripts/jquery/jquery.min.js"></script>
-    <script type="text/javascript" language="JavaScript" src="/controls/menu/ddsmoothmenu.js"></script>
-    <script type="text/javascript" language="JavaScript" src="/admin/category/list/script/list_categories.js"></script>
-    <script type="text/javascript" language="javascript"
-            src="/controls/grid/datatables-grid/js/jquery.dataTables.js"></script>
-    <link rel="stylesheet" type='text/css' href='/controls/menu/ddsmoothmenu.css'/>
-    <link rel="stylesheet" type='text/css' href='/controls/menu/ddsmoothmenu-v.css'/>
-    <link rel="stylesheet" type="text/css" href="/style/css/list.css"/>
+    <title>UAI - Boletín Científico - Tecnológico</title>
+
+    <script type="text/javascript" src="/node_modules/jquery/dist/jquery.min.js"></script>
+    <script type="text/javascript" src="/node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="/scripts/home/home.js"></script>
+    <link rel="stylesheet" href="/node_modules/bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/node_modules/font-awesome/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="/style/css/general.css" media="screen"/>
 </head>
 
-<body id="dt_example">
-<div id="smoothmenu1" class="ddsmoothmenu"></div>
-<h1>Categorías</h1>
-<div>
+<body>
 
-    <div id="container">
-
-        <div id="output"></div>
-
-        <div id="demo">
-            <table cellpadding="0" cellspacing="0" border="0" class="display" id="example">
-                <thead>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Estado</th>
-                    <th>Borrar</th>
-                </tr>
-                </thead>
-                <tbody>
-
-                <?php
-
-                try {
-                    $rs = $dbSetting->ExecuteQuery(__QUERY_GET_ALL_CATEGORIES_ORDER_BY_NAME__);
-
-                    while ($row = mysql_fetch_assoc($rs)) {
-                        echo "<tr class='center'>" .
-                            "<td class='center'> {$row['name']} </td>" .
-                            "<td class='center'> <input id='button_change_{$row['id']}' type='button' value='" . GetButtonDescription($row["status"]) . "' onclick='ChangeStatus(" . $row['id'] . "," . GetStatus($row['status']) . ")' style='width:150px;' /> </td>" .
-                            "<td class='center'> <input id='button_delete_{$row['id']}' type='button' value='Borrar' onclick='Delete(" . $row['id'] . ")' style='width:150px;'/>" . "</tr>";
-                    }
-
-                } catch (Exception $e) {
-                    echo $e;
-                }
-
-
-                function GetButtonDescription($status)
-                {
-                    if ($status == 0) {
-                        return "Habilitar";
-                    }
-                    {
-                        return "Desabilitar";
-                    }
-                }
-
-                function GetStatus($status)
-                {
-                    if ($status == 0) {
-                        return 1;
-                    } else {
-                        return 0;
-                    }
-                }
-
-                ?>
-                </tbody>
-                <tfoot>
-                <tr>
-                    <th>Nombre</th>
-                    <th>Estado</th>
-                    <th>Borrar</th>
-                </tr>
-                </tfoot>
-            </table>
-
+<div class="container">
+    <div class="row">
+        <!-- Header and Navbar -->
+        <div class="col-md-12">
+            <div class="page-header">
+                <?php require_once '../../../controls/menu/menu_nav.php'; ?>
+                <?php require_once '../../../controls/header/widget.php'; ?>
+            </div>
         </div>
-        <div class="spacer"></div>
+        <!-- End Header and Navbar -->
+        <div>
+            <div id="container">
+                <table class="table">
+                    <thead>
+                    <tr>
+                        <th>Nombre</th>
+                        <th>Estado</th>
+                        <th>Borrar</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
 
+                    try {
+                        $rs = $dbSetting->ExecuteQuery(__QUERY_GET_ALL_CATEGORIES_ORDER_BY_NAME__);
+
+                        while ($row = mysql_fetch_assoc($rs)) {
+                            echo "<tr class='center'>" .
+                                "<td class='center'> {$row['name']} </td>" .
+                                "<td class='center'> <input id='button_change_{$row['id']}' type='button' value='" . GetButtonDescription($row["status"]) . "' onclick='ChangeStatus(" . $row['id'] . "," . GetStatus($row['status']) . ")' style='width:150px;' /> </td>" .
+                                "<td class='center'> <input id='button_delete_{$row['id']}' type='button' value='Borrar' onclick='Delete(" . $row['id'] . ")' style='width:150px;'/>" . "</tr>";
+                        }
+
+                    } catch (Exception $e) {
+                        echo $e;
+                    }
+
+
+                    function GetButtonDescription($status)
+                    {
+                        if ($status == 0) {
+                            return "Habilitar";
+                        }
+                        {
+                            return "Desabilitar";
+                        }
+                    }
+
+                    function GetStatus($status)
+                    {
+                        if ($status == 0) {
+                            return 1;
+                        } else {
+                            return 0;
+                        }
+                    }
+
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
-
 </div>
-
-<script type="text/javascript">
-
-    var _gaq = _gaq || [];
-    _gaq.push(['_setAccount', 'UA-10081016-2']);
-    _gaq.push(['_trackPageview']);
-
-    (function () {
-        var ga = document.createElement('script');
-        ga.type = 'text/javascript';
-        ga.async = true;
-        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(ga, s);
-    })();
-
-</script>
 </body>
 </html>
